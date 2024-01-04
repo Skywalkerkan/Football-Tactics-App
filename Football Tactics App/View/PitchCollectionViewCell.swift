9b5ed6099a52a6320f111617232a7b48c7bfcc31
Skywalkerkan <cosarerkann>
2024-01-04 17:11:36 +0300

    CollectionViewCell

---
+++ Football Tactics App/View/PitchCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PitchCollectionViewCell.swift
+//  Football Tactics App
+//
+//  Created by Erkan on 4.01.2024.
+//
+
+import UIKit
+
+class PitchCollectionViewCell: UICollectionViewCell {
+    
+}
