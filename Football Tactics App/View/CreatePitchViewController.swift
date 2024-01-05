0eb47002ee1236a55d3003108e4b2a9376e5f9be
Skywalkerkan <cosarerkann>
2024-01-05 23:19:06 +0300

    Creating Pitch Controller

---
+++ Football Tactics App/View/CreatePitchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreatePitchViewController.swift
+//  Football Tactics App
+//
+//  Created by Erkan on 4.01.2024.
+//
+
+import UIKit
+
+class CreatePitchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
