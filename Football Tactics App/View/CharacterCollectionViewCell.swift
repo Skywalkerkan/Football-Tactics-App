a523a40991c8d99c9649d65c901937b5a6e3b4b2
Skywalkerkan <cosarerkann>
2024-01-26 14:06:16 +0300

    PlayerNoAdded

---
+++ Football Tactics App/View/CharacterCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CharacterCollectionViewCell.swift
+//  Football Tactics App
+//
+//  Created by Erkan on 25.01.2024.
+//
+
+import UIKit
+
+class CharacterCollectionViewCell: UICollectionViewCell {
+    
+}
