57b79a35f7a1af96ea1f4d6ac08e77e7ead5508c
Skywalkerkan <cosarerkann>
2024-01-29 02:23:31 +0300

    CharacterCollectionViewCell

---
+++ Football Tactics App/View/Cells/LanguagesTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  LanguagesTableViewCell.swift
+//  Football Tactics App
+//
+//  Created by Erkan on 28.01.2024.
+//
+
+import UIKit
+
+class LanguagesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
